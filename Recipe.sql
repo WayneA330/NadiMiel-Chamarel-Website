48924ff0fa25de620edf651820b18d10a054fda3
oliverahyou <ahyouoliver9@gmail.com>
2022-04-29 12:36:11 +0400

    Recipe sql

---
+++ Recipe.sql
@@ -0,0 +1,79 @@
+-- INSERT into recipe (recipe_name_fr, recipe_name_eng, recipe_description_fr, recipe_description_eng, recipe_article_fr, recipe_article_eng)
+-- VALUES ('Recette Cassoulet fait maison version Nadi’Miel Chamarel',
+-- 		'Homemade Cassoulet recipes version Nadi’Miel Chamarel',
+-- 		'Ingrédients:
+-- 		Haricots blancs 500 g
+-- 		Boucané Nadi’Miel Chamarel 250 g
+-- 		Saucisses Porc Combava Nadi’Miel Chamarel x 2
+-- 		Saucisses fumées Nadi’Miel Chamarel x 2
+-- 		Poulet fumé Nadi’Miel Chamarel (700g)
+-- 		1 botte de thym
+-- 		4 Feuilles de laurier
+-- 		4 gousses d’ail
+-- 		Deux gros Onions
+-- 		Trois grosses tomates x 3
+-- 		Huile (3 c. à bouche)
+-- 		500 ml d’eau
+-- 		Curcumin (1 c. à café)
+-- 		Miel (1 c. à bouche)
+-- 		Sel selon vos goûts
+-- 		' ,
+-- 	   	'Ingredients: 
+-- 		White beans 500 g 
+-- 		Boucané Nadi’Miel Chamarel 250 g 
+-- 		Sausages Pork Combava Nadi’Miel Chamarel x 2 
+-- 		Smoked sausages Nadi’Miel Chamarel x 2 
+-- 		Smoked chicken Nadi’Miel Chamarel (700g) 
+-- 		1 bunch of thyme 
+-- 		4 Bay leaves 
+-- 		4 cloves garlic 
+-- 		Two big Onions 
+-- 		Three large tomatoes x 3 
+-- 		Oil (3 tbsp) 
+-- 		500 ml of water Curcumin (1 tsp)
+-- 		Honey (1 tbsp.) 
+-- 		Salt according to your tastes',
+-- 	   	'Préparation:
+-- 		Bouillir le boucané et les saucisses pendant 10 minutes.
+-- 		Cuire les haricots dans une cocotte minute
+-- 		Emincer les oignons
+-- 		Découper les tomates en cube
+-- 		Découper le poulet en quatre
+-- 		Couper les saucisses
+
+-- 		Cuisson:
+-- 		Faites revenir le boucané et les saucisses
+-- 		Ajouter un peu d’huile
+-- 		Ajouter les oignons, l’ail, le thym et le curcumin
+-- 		Ensuite ajouter le poulet, les tomates et les feuilles de laurier
+-- 		Laissez cuire pendant 10 minutes en remuant de temps en temps
+-- 		Ajouter les haricots blanc avec 500 ml d’eau.
+-- 		Laisser mijoter pendant 25 à 30 minutes à feu doux.
+-- 		Saler selon vos goûts
+-- 		Après la cuisson ajouter le miel.
+
+-- 		Servir accompagné de riz ou du pain et d’un achard Nadi’Miel Chamarel ou d’un Piment Nadi’Miel Chamarel.
+-- 		Bon appétit', 
+-- 	  'Preparation: 
+-- 		Boil the boucané and sausages for 10 minutes. 
+-- 		Cook the beans in a pressure cooker 
+-- 		Slice the onions 
+-- 		Cut the tomatoes into cubes 
+-- 		Cut the chicken in four Cut the sausages 
+
+-- 		Cooking:
+-- 		Sauté the boucané and sausages 
+-- 		Add a little oil 
+-- 		Add the onions, garlic, thyme and curcumin 
+-- 		Then add the chicken, tomatoes and bay leaves 
+-- 		Cook for 10 minutes, stirring occasionally 
+-- 		Add the white beans with 500 ml of water. 
+-- 		Simmer for 25 to 30 minutes over low heat. 
+-- 		Salt according to your tastes
+-- 		After cooking add the honey. 
+
+-- 		Serve with rice or bread and a Nadi’Miel Chamarel achard or a Nadi’Miel Chamarel pepper. 
+-- 		Enjoy your meal');
+
+DELETE FROM recipe WHERE recipe_id = 2;
+SELECT * FROM recipe;
